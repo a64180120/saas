c31ff396b4a8e6dbcd87d5c6aabfb06aba6fcb62
wufm <542171065@qq.com>
2018-11-24 22:29:03 +0800

    新增 组织管理的 编辑页面

--- src/api/user/userInfo.js
+++ src/api/user/userInfo.js
@@ -1,12 +1,17 @@
 
 /** 
+ * 说明: 新增
  * object 当前对象 vue
  * param 参数集合
 */
-export function SysUserAdd(object,param) {
+export function SysUserAddUp(object,param) {
   return new Promise((resolve, reject) => {
+    // let url='/SysUser/PostAdd'
+    // if(param.otype==='edit'){
+    //     url='/SysUser/PostAdd'
+    // }
     object.$axios({
-          url: "/SysUser/PostAdd",
+          url: '/SysUser/PostAdd',
           method: "post",
           data: param,
       }).then(res => {
@@ -18,6 +23,7 @@ export function SysUserAdd(object,param) {
       });
   });
 }
+
 /** 
  * 说明:用户信息列表的方法
  * object 当前对象 vue
@@ -37,3 +43,24 @@ export function SysUserList(object,param) {
     });
   });
 }
+
+/** 
+ * 说明:删除
+ * object 当前对象 vue
+ * param 参数集合
+*/
+export function SysUserDelete(object,param) {
+    return new Promise((resolve, reject) => {
+      object.$axios({
+            url: "/SysUser/PostDelete",
+            method: "post",
+            data: param,
+        }).then(res => {
+            //正确
+            resolve(res);               
+        }).catch(error => {
+            //错误
+            reject(error);
+        });
+    });
+  }
